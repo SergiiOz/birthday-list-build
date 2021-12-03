7280dc32aa4bc441e1aff7fd977cbdfadef2f85c
Sergii Ozinkovskyi <s.ozinkovskyi@gmail.com>
2021-12-03 23:16:00 +0200

    added tag <form> in EmloyeesCard -> fix radio button

--- src/components/EmployeeCard.js
+++ src/components/EmployeeCard.js
@@ -1,10 +1,10 @@
-import PropTypes from "prop-types";
-import { useState } from "react";
-import { useDispatch } from "react-redux";
-import { selectEmployee, unSelectEmployee } from "./../actions/actionCreators";
+import PropTypes from 'prop-types';
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { selectEmployee, unSelectEmployee } from './../actions/actionCreators';
 
 const EmployeeCard = ({ employee }) => {
-  const [isActive, setIsActive] = useState("false");
+  const [isActive, setIsActive] = useState('false');
   const dispatch = useDispatch();
 
   //get change from radio buttons
@@ -12,50 +12,51 @@ const EmployeeCard = ({ employee }) => {
     setIsActive(event.target.value);
 
     //add employee in Birthday list
-    if (event.target.value === "true") {
+    if (event.target.value === 'true') {
       dispatch(selectEmployee(employee));
     }
     //remove employee from Birthday list
-    if (event.target.value === "false") {
+    if (event.target.value === 'false') {
       dispatch(unSelectEmployee(employee));
     }
   };
 
   return (
     <div className="employee-card">
-      <h4 className={isActive === "true" ? "active" : ""}>
+      <h4 className={isActive === 'true' ? 'active' : ''}>
         {employee.firstName} {employee.lastName}
       </h4>
       {/* RADIO BUTTONS */}
       {/* doesn't work ui checked radio button- need press doubleClick */}
       <div>
-        <div>
-          <label>
-            <input
-              type="radio"
-              value="false"
-              name="status"
-              checked={isActive === "false"}
-              onChange={onChangeStatus}
-            />{" "}
-            Not Active
-          </label>
-        </div>
+        <form>
+          <div>
+            <label>
+              <input
+                type="radio"
+                value="false"
+                name="status"
+                checked={isActive === 'false'}
+                onChange={onChangeStatus}
+              />{' '}
+              Not Active
+            </label>
+          </div>
 
-        <div>
-          <label>
-            <input
-              type="radio"
-              value="true"
-              name="status"
-              checked={isActive === "true"}
-              onChange={onChangeStatus}
-            />{" "}
-            Active
-          </label>
-        </div>
+          <div>
+            <label>
+              <input
+                type="radio"
+                value="true"
+                name="status"
+                checked={isActive === 'true'}
+                onChange={onChangeStatus}
+              />{' '}
+              Active
+            </label>
+          </div>
+        </form>
       </div>
-      {isActive}
     </div>
   );
 };
